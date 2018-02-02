063f491483380cbf9f430609cb6af739d40c22ea
Sayed Mohammed Tasmimul Huda <34653977+tasmimul-huda@users.noreply.github.com>
2018-02-03 01:56:38 +0600

    Create arduni.ino

---
+++ arduni.ino
@@ -0,0 +1,242 @@
+#define iop1 3
+#define iop2 5
+#define LED1 6  //AND GATE OUTPUT
+#define LED2 7 //NAND GATE OUTPUT
+#define LED3 8 //OR GATE OUTPUTy
+#define LED4 9  //NOR GATE OUTPUT
+#define LED5 10 //NAOT GATE OUTPUT
+#define LED6 11  //X-OR GATE OUTPUT
+void setup()
+{
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(LED1,INPUT);
+  pinMode(LED2,INPUT);
+  pinMode(LED3,INPUT);
+  pinMode(LED4,INPUT);
+  pinMode(LED5,INPUT);
+  pinMode(LED6,INPUT);
+}
+void loop()
+{
+  Serial.begin(9600);
+  delay(5000);
+  int L1=digitalRead(6);
+  int L2=digitalRead(7);
+  int L3=digitalRead(8);
+  int L4=digitalRead(9);
+  int L5=digitalRead(10);
+  int L6=digitalRead(11);
+  
+  Serial.println(' ');
+  Serial.print("input1=0 input2=0");
+  Serial.println(' ');
+  digitalWrite(5,LOW);
+  digitalWrite(3,LOW);
+  Serial.print("AND GATE: ");
+   if(digitalRead(6)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(6)==LOW){
+    Serial.print("OFF");
+}
+   Serial.println(' ');
+    Serial.print("NAND GATE: ");
+    Serial.println(' ');
+   if(digitalRead(7)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(7)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(8)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(8)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(9)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(9)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOT GATE: ");
+  Serial.println(' ');
+   if(digitalRead(10)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(10)==LOW){
+    Serial.print("OFF");
+   }
+   Serial.println(' ');
+  Serial.print("X-OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(11)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(11)==LOW){
+    Serial.print("OFF");
+   }
+    delay(6000);
+    delay(8000);
+ Serial.println(' ');
+  Serial.print("input1=1 input2=0");
+  Serial.println(' ');
+  digitalWrite(5,HIGH);
+  digitalWrite(3,LOW);
+  Serial.print("AND GATE: ");
+   if(digitalRead(6)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(6)==LOW){
+    Serial.print("OFF");
+}
+     Serial.println(' ');
+    Serial.print("NAND GATE: ");
+    Serial.println(' ');
+   if(digitalRead(7)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(7)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(8)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(8)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(9)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(9)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOT GATE: ");
+  Serial.println(' ');
+   if(digitalRead(10)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(10)==LOW){
+    Serial.print("OFF");
+   }
+   Serial.println(' ');
+  Serial.print("X-OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(11)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(11)==LOW){
+    Serial.print("OFF");
+   }
+    delay(6000);
+    delay(8000);
+    Serial.println(' ');
+    Serial.print("input1=0 input2=1");
+    Serial.println(' ');
+    digitalWrite(5,LOW);
+    digitalWrite(3,HIGH);
+    Serial.print("AND GATE: ");
+   if(digitalRead(6)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(6)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+    Serial.print("NAND GATE: ");
+    Serial.println(' ');
+   if(digitalRead(7)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(7)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(8)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(8)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(9)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(9)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOT GATE: ");
+  Serial.println(' ');
+   if(digitalRead(10)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(10)==LOW){
+    Serial.print("OFF");
+   }
+   Serial.println(' ');
+  Serial.print("X-OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(11)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(11)==LOW){
+    Serial.print("OFF");
+   }
+    delay(6000);
+    delay(8000);
+ Serial.println(' ');
+    Serial.print("input1=1 input2=1");
+    Serial.println(' ');
+  digitalWrite(5,HIGH);
+  digitalWrite(3,HIGH);
+  Serial.print("AND GATE: ");
+   if(digitalRead(6)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(6)==LOW){
+    Serial.print("OFF");
+}   Serial.println(' ');
+    Serial.print("NAND GATE: ");
+    Serial.println(' ');
+   if(digitalRead(7)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(7)==LOW){
+    Serial.print("OFF");
+} Serial.println(' ');
+  Serial.print("OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(8)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(8)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(9)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(9)==LOW){
+    Serial.print("OFF");
+}
+Serial.println(' ');
+  Serial.print("NOT GATE: ");
+  Serial.println(' ');
+   if(digitalRead(10)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(10)==LOW){
+    Serial.print("OFF");
+   }
+  Serial.println(' ');
+  Serial.print("X-OR GATE: ");
+  Serial.println(' ');
+   if(digitalRead(11)==HIGH)
+    Serial.print("ON");
+   else if(digitalRead(11)==LOW){
+    Serial.print("OFF");
+   }
+    delay(6000);
+    delay(8000);
+ 
+}
